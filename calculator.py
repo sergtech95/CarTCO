269aad0e388adda36d4f16f93f50b9f3e83cc773
sergtech95 <s.lutsenko08@gmail.com>
2024-01-07 15:30:46 -0500

    initital commit

--- calculator.py
+++ calculator.py
@@ -2,60 +2,59 @@ import api
 
 
 class Calculator:
-    def __init__(self, mileage=12000, years=3, depreciation_oer_year=19):
+    def __init__(self, mileage=15000, years=3, year_loss=10):
         self.mileage = mileage
+        self.cars = {} # Car : Year cost
         self.years = years
-        self.depreciation_per_year = depreciation_oer_year / 100
-        self.vehicles = {}  # Vehicle : Annual_cost
+        self.year_loss = year_loss / 100
 
-    def add_vehicle(self, vehicle):
-        year_cost = vehicle.total_annual_cost(self.mileage)
-        price_per_year = vehicle.price / self.years
-        price_after_depreciation = self.get_price_after_depreciation(vehicle) / self.years
-        self.vehicles[vehicle] = year_cost + price_per_year - price_after_depreciation
+    def add_car(self, car):
+        year_cost = car.year_cost(self.mileage)
+        price_per_year = car.price / self.years
+        left_price = self.get_left_price(car) / self.years
+        self.cars[car] = year_cost + price_per_year - left_price
 
-    def get_price_after_depreciation(self, vehicle):
-        initial_price = vehicle.price
+    def get_left_price(self, car):
+        initial_price = car.price
         for i in range(self.years):
-            initial_price -= initial_price * self.depreciation_per_year
+            initial_price -= initial_price * self.year_loss
         return initial_price
 
-    def print_vehicles(self):
-        for vehicle, total_annual_cost in self.vehicle.items():
-            print(f"{vehicle.name}:    {total_annual_cost}:")
+    def print_cars(self):
+        for car, year_price, in self.cars.items():
+            print(f"{car.name}:\t\t {year_price}")
 
 
-class Vehicle:
+class Car:
     def __init__(self,
                  name: str,
                  price: int,
                  fuel_economy: float,
-                 maintenance_cost: int,
+                 service_cost: int,
                  insurance_cost: int):
         self.name = name
         self.price = price
         self.fuel_economy = fuel_economy
-        self.maintenance_cost = maintenance_cost
+        self.service_cost = service_cost
         self.insurance_cost = insurance_cost
 
-    def static_annual_cost(self):
-        return self.maintenance_cost + self.insurance_cost
+    def static_year_cost(self):
+        return self.service_cost + self.insurance_cost
 
-    def dynamic_annual_cost(self, mileage: int):
+    def dynamic_year_cost(self, mileage: int):
         return self.fuel_economy * mileage / 100 * api.get_gas_price()
 
-    def total_annual_cost(self, mileage: int):
-        return self.static_annual_cost + self.dynamic_annual_cost(mileage)
+    def year_cost(self, mileage: int):
+        return self.static_year_cost() + self.dynamic_year_cost(mileage)
 
 
-class ElectricVehicle(Vehicle):
-    def __init__(self,
-                 name: str,
-                 price: int,
-                 insurance_cost: int,
-                 power_consumption: int):
-        super().__init__(name=name, price=price, fuel_economy=0, maintenance_cost=0, insurance_cost=insurance_cost)
+class ElectricCar(Car):
+    def __init__(self, name: str, price: int, insurance_cost: int, power_consumption: int):
+        super().__init__(name=name, price=price, fuel_economy=0, service_cost=0, insurance_cost=insurance_cost)
         self.power_consumption = power_consumption
 
-    def dynamic_annual_cost(self, mileage: int):
+    def dynamic_year_cost(self, mileage: int):
         return self.power_consumption * mileage / 1000 * api.get_kwt_price()
+
+
+
--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import calculator
 
 if __name__ == '__main__':
     calc = calculator.Calculator()
-    calc.add_vehicle(
-        calculator.Vehicle("BMW 7 series",
-                           45000, 14, 1200, 2500),
+    calc.add_car(
+        calculator.Car("Range Rover Velar",
+                       35000,
+                       25,
+                       1200,
+                       2500),
     )
-    calc.add_vehicle(
-        calculator.Vehicle("RR Sport",
-                           50000, 10, 1800, 2500),
+    calc.add_car(
+        calculator.Car("Range Rover Sport",
+                       50000,
+                       18,
+                       1800,
+                       2500),
     )
-    calc.add_vehicle(
-        calculator.ElectricVehicle("Tesla Model 3",
-                                   30000, 2000, 150),
+    calc.add_car(
+        calculator.ElectricCar("Tesla Model 3",
+                               30000,
+                               2000,
+                               150),
     )
 
-    calc.print_vehicles()
+    calc.print_cars()
